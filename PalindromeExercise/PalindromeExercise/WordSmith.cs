997fbbf84b0549aee8e4b1f1c051c9d14007faaf
owen mei <owenzackmei@gmail.com>
2023-07-14 00:37:17 -0400

    init commit

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string str)
+        {
+            return string.Join("", str.Reverse()) == str;
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,22 @@
 using System;
 using Xunit;
+using PalindromeExercise;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("basinab", false)]
+        [InlineData("mushsum", true)]
+        [InlineData("asdfklwefjnasodfiasdhjiugfh4ewiulhdsakd", false)]
+        [InlineData("a", true)]
+        public void IsAPalindromeTest(string str, bool expected)
         {
-
+            WordSmith newSmith = new WordSmith();
+            bool actual = newSmith.IsAPalindrome(str);
+            Assert.Equal(expected, actual);
         }
     }
 }
